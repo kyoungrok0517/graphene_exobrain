d58e4a66af4d58b69331d2e5c4bf6171e15e45d3
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-10-02 23:50:40 +0000

    enable isolateSentences option

--- src/main/java/GrapheneWorker.java
+++ src/main/java/GrapheneWorker.java
@@ -40,7 +40,7 @@ public class GrapheneWorker implements Runnable {
                         continue;
                     }
                     try {
-                        json = this.graphene.doRelationExtraction(line, false, false).serializeToJSON();
+                        json = this.graphene.doRelationExtraction(line, false, true).serializeToJSON();
                         fileName = filePath.getFileName().toString();
                         outQueue.add(fileName + '\t' + json);
                     } catch (RuntimeInterruptedException e) {
